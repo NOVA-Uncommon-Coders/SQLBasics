0ce04c2a4ac388f3ae9e7a517470a49d95154b44
Steve Jobs <peter@Steves-MBP.home>
2017-03-11 14:47:25 -0500

    crd works

--- src/com/theironyard/novauc/Main.java
+++ src/com/theironyard/novauc/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public static void insertRestaurant(String restName, String bestDish, int restNumWaiters) throws SQLException {
         PreparedStatement ps = getConnection().prepareStatement
                 ("INSERT INTO restaurants (restName, bestDish, restNumWaiters) VALUES (?, ?, ?)");
-        User user = currentUser();
+        //User user = currentUser();
         ps.setString(1,restName);
         ps.setString(2,bestDish);
         ps.setInt(3,restNumWaiters);
@@ -30,6 +30,37 @@ public class Main {
         Statement stated = getConnection().createStatement();
         stated.execute("CREATE TABLE IF NOT EXISTS restaurants (id IDENTITY, restName VARCHAR, bestDish VARCHAR, restNumWaiters INT )");
         stated.execute("CREATE TABLE IF NOT EXISTS user (id IDENTITY , userName VARCHAR, password VARCHAR)");
+    }
+
+    public static void deleteRestaurant(int id) throws SQLException {
+        PreparedStatement ps = getConnection().prepareStatement("DELETE FROM restaurants WHERE id=?");
+        ps.setInt(1,id);
+        ps.execute();
+    }
+
+    public static ArrayList<Restaurant> selectRestaurants() throws SQLException {
+        PreparedStatement ps = getConnection().prepareStatement("SELECT * FROM restaurants ");
+        ArrayList<Restaurant> restaurantsAL = new ArrayList<>();
+        ResultSet results = ps.executeQuery();
+        while (results.next()){
+            int id = results.getInt("id");
+            String restName = results.getString("restName");
+            String bestDish = results.getString("bestDish");
+            int restNumWaiters = results.getInt("restNumWaiters");
+
+            restaurantsAL.add(new Restaurant(id, restName, bestDish, restNumWaiters));
+        }
+        return restaurantsAL;
+    }
+
+    public static void updateRestaurant(String restName, String bestDish, int restNumWaiters) throws SQLException {
+        PreparedStatement ps = getConnection().prepareStatement
+                ("UPDATE restaurants SET (restName, bestDish, restNumWaiters) VALUES (?, ?, ?)");
+        //User user = currentUser();
+        ps.setString(1,"restEditName");
+        ps.setString(2,"restEditDish");
+        ps.setInt(3,"restEditDish");
+        ps.execute();
     }
 
     public static User currentUser() {
@@ -53,7 +84,7 @@ public class Main {
                 return new ModelAndView(userActivity,"index.html");
                 }
             else {
-                  userActivity.put("entries", restAL);
+                  userActivity.put("entries", selectRestaurants());
                   userActivity.put("userName", name);
                   return new ModelAndView(userActivity, "index.html");
                   }
@@ -79,27 +110,27 @@ public class Main {
             return "";
         });
 
-        Spark.post("/create-restaurant", ((request, response) -> {
-            Session session = request.session();
-            String name = session.attribute("userName");
+        Spark.post("/create-restaurant", (request, response) -> {
+//            Session session = request.session();
+//            String name = session.attribute("userName");
 
             String restName = request.queryParams("restName");
             String bestDish = request.queryParams("bestDish");
             int restNumWaiters = Integer.valueOf(request.queryParams("restNumWaiters"));
 
-            Restaurant entryObj = new Restaurant(restName, bestDish, restNumWaiters);
-            restAL.add(entryObj);
+//            Restaurant entryObj = new Restaurant(restName, bestDish, restNumWaiters);
+//            restAL.add(entryObj);
 
             insertRestaurant(restName, bestDish,restNumWaiters);
 
             response.redirect("/");
             return "";
-        }));
+        });
 
         Spark.post("/edit-restaurant", (request, response) -> {
-            String editor = request.queryParams("editMessageT");
+            //String editor = request.queryParams("editMessageT");
 
-            int edit = Integer.valueOf(request.queryParams("messID"));
+            //int edit = Integer.valueOf(request.queryParams("messID"));
 
             Restaurant entrance = null;
             for (Restaurant picker : restAL) {
@@ -111,40 +142,42 @@ public class Main {
             if (entrance != null && editor != null){
                 //entrance.setText(editor);
             }
+
+
             response.redirect("/");
             return "";
         });
 
-        /*
-        Spark.get("/anotherplace/:id", ((request, response) -> {
-                    String idJunk = request.params("id");
-                    HashMap whatever = new HashMap();
-                    whatever.put("id",idJunk);
-                    return new ModelAndView(whatever, "anotherplace.html");
-                }), new MustacheTemplateEngine()
-        );
-
-       */
+//        Spark.get("/anotherplace/:id", ((request, response) -> {
+//                    String idJunk = request.params("id");
+//                    HashMap whatever = new HashMap();
+//                    whatever.put("id",idJunk);
+//                    return new ModelAndView(whatever, "anotherplace.html");
+//                }), new MustacheTemplateEngine()
+//        );
 
         Spark.post("/delete-restaurant", (request, response) -> {
-            int delete = Integer.valueOf(request.queryParams("messDel"));
-            Restaurant entrance = new Restaurant();
-            for (Restaurant picker : restAL) {
-                if (/*picker.getId()*/ 1 ==  delete) {
-                    entrance = picker;
-                }
-            }
-            restAL.remove(entrance);
+//            int delete = Integer.valueOf(request.queryParams("restDelete"));
+//            Restaurant entrance = new Restaurant();
+//            for (Restaurant picker : restAL) {
+//                if (/*picker.getId()*/ 1 ==  delete) {
+//                    entrance = picker;
+//                }
+//            }
+//            restAL.remove(entrance);
+            
+            deleteRestaurant(Integer.valueOf(request.queryParams("restDelete")));
+
             response.redirect("/");
             return "";
         });
 
-        Spark.post("/logout", ((request, response) -> {
+        Spark.post("/logout", (request, response) -> {
             Session session = request.session();
             session.invalidate();
             response.redirect("/");
             return "";
-        }));
+        });
     }
 }
 
--- src/com/theironyard/novauc/Restaurant.java
+++ src/com/theironyard/novauc/Restaurant.java
@@ -1,11 +1,13 @@
 package com.theironyard.novauc;
 
 public class Restaurant {
+    private int id;
     private String restName;
     private String bestDish;
     private int restNumWaiters;
 
-    public Restaurant(String restName, String bestDish, int restNumWaiters){
+    public Restaurant(int id, String restName, String bestDish, int restNumWaiters){
+        this.id = id;
         this.restName = restName;
         this.bestDish = bestDish;
         this.restNumWaiters = restNumWaiters;
@@ -13,13 +15,13 @@ public class Restaurant {
 
     public Restaurant() {}
 
+    public int getId() {return id; }
+
     public String getRestName() {
         return restName;
     }
 
-    public String getBestDish() {
-        return bestDish;
-    }
+    public String getBestDish() {return bestDish; }
 
     public int getRestNumWaiters() {
         return restNumWaiters;
